b7776a6ec2a330ffa79faf9d020e8a36393b93cc
Programming-Sai <saiahprog6@gmail.com>
2026-08-05 03:34:09 +0000

    fixing trigger bug

--- hello-clice/check.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-# ubuntu:22.04 (hello-clice's base image) has no perl installed, so this
-# checker should fail loudly and clearly when exec'd - not silently, and
-# not with a stack trace that looks like a clice bug rather than a missing
-# interpreter.
-print "This should never run - perl isn't in the image.\n";
-exit(0);
